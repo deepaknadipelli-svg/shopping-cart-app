29ba309108ec4d18db99ba0c20321f144baa7fc6
Sai Kiran Deepak Rao <deepaknadipelli@gmail.com>
2026-02-06 23:51:43 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ const cartRoutes = require("./routes/cartRoutes");
 const orderRoutes = require("./routes/orderRoutes");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
@@ -17,7 +18,12 @@ app.use("/api/items", itemRoutes);
 app.use("/api/carts", cartRoutes);
 app.use("/api/orders", orderRoutes);
 
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => app.listen(5000, () => console.log("Server running on port 5000")))
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
   .catch(err => console.error(err));
